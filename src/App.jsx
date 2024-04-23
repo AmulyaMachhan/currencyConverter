9559dcf2653923bfd0658fd74dd2495d8fe22a0c
AmulyaMachhan <machhanamulya@gmail.com>
2024-04-24 03:32:18 +0530

    fix the bug related to the input box

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 function App() {
 
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState("")
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
-  const [convertedAmount, setConvertedAmount] = useState(0)
+  const [convertedAmount, setConvertedAmount] = useState("")
 
   const currencyInfo = useCurrencyInfo(from)
 
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -27,7 +27,7 @@ function InputBox({
                     placeholder="Amount"
                     disabled={amountDisable}
                     value={amount}
-                    onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                    onChange={(e) => onAmountChange && onAmountChange((e.target.value))}
                 />
             </div>
             <div className="w-1/2 flex flex-wrap justify-end text-right">
